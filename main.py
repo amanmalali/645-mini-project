5e5e269ca5d9431e1716c6216708eba62b672d40
Aman Malali <amanmalali@wpa145.wpanet.cs.local>
2024-05-09 18:40:48 -0400

    Fixed bugs

--- main.py
+++ main.py
@@ -46,32 +46,82 @@ def run_kmeans(data_df,k=6):
     labels=km.fit_predict(data_df)
     return km
 
-def find_n_obj_kmeans(km,data_x,num=15,use_dist=False):
+def find_n_obj_kmeans(km,data_x,num=15,full_data=None,use_dist=False):
     centroids  = km.cluster_centers_
     distances = pairwise_distances(centroids, data_x, metric='euclidean')
-    ind = [np.argpartition(i, num)[:num] for i in distances]
-    y=-10
+    y=10
+    f=10
     if use_dist:
         fixed_ind=[]
         cluster_labels=np.array(km.predict(data_x))
         for c in range(len(centroids)):
+            dim_min_max={}
             cluster_idx = np.where(cluster_labels == c)[0]
-            cluster_idx = list(cluster_idx)
-            dist_c=distances[c]
-            distances_cluster=dist_c[cluster_idx]
-            max_dist_c=max(distances_cluster)+y
-            dist_c=np.array(dist_c)
-            dataset_idx=np.argwhere(dist_c<max_dist_c).squeeze(1).tolist()
-            fixed_ind.extend(dataset_idx)
+            cluster_size=len(cluster_idx)
+            data_x_c=data_x.iloc[cluster_idx]
+            dims=data_x.columns
+            for dim in dims:
+                dim_min_max[dim]=[min(data_x_c[dim])-y,max(data_x_c[dim]+y)]
+            rect_data=full_data.loc[(full_data[dims[0]]<dim_min_max[dims[0]][1]) & 
+                          (full_data[dims[1]]<dim_min_max[dims[1]][1]) &
+                          (full_data[dims[2]]<dim_min_max[dims[2]][1]) &
+                          (full_data[dims[3]]<dim_min_max[dims[3]][1]) &
+                          (full_data[dims[4]]<dim_min_max[dims[4]][1]) &
+                          (dim_min_max[dims[0]][0]<full_data[dims[0]]) &
+                          (dim_min_max[dims[1]][0]<full_data[dims[1]]) &
+                          (dim_min_max[dims[2]][0]<full_data[dims[2]]) &
+                          (dim_min_max[dims[3]][0]<full_data[dims[3]]) &
+                          (dim_min_max[dims[4]][0]<full_data[dims[4]])]
+            
+            # cluster_idx = list(cluster_idx)
+            # dist_c=distances[c]
+            # distances_cluster=dist_c[cluster_idx]
+            # max_dist_c=max(distances_cluster)+y
+            # dist_all_c=np.array(distances_all[c])
+            # dataset_idx=np.argwhere(dist_all_c<max_dist_c).squeeze(1).tolist()
+            fc_data=rect_data.sample(f*cluster_size)
+            fixed_ind.extend(fc_data.index)
         
         fixed_ind=np.unique(fixed_ind)
-        print(len(fixed_ind))
     else:
+        ind = [np.argpartition(i, num)[:num] for i in distances]
         fixed_ind=[]
         for i in ind:
             fixed_ind.append(i[0])
     return fixed_ind
 
+def find_n_obj_data(data_x,full_data=None):
+    y=10
+    f=10
+    fixed_ind=[]
+    for c in range(len(data_x)):
+        dim_min_max={}
+        dims=data_x.columns
+        for dim in dims:
+            dim_min_max[dim]=[data_x.iloc[c][dim]-y,data_x.iloc[c][dim]+y]
+        rect_data=full_data.loc[(full_data[dims[0]]<dim_min_max[dims[0]][1]) & 
+                        (full_data[dims[1]]<dim_min_max[dims[1]][1]) &
+                        (full_data[dims[2]]<dim_min_max[dims[2]][1]) &
+                        (full_data[dims[3]]<dim_min_max[dims[3]][1]) &
+                        (full_data[dims[4]]<dim_min_max[dims[4]][1]) &
+                        (dim_min_max[dims[0]][0]<full_data[dims[0]]) &
+                        (dim_min_max[dims[1]][0]<full_data[dims[1]]) &
+                        (dim_min_max[dims[2]][0]<full_data[dims[2]]) &
+                        (dim_min_max[dims[3]][0]<full_data[dims[3]]) &
+                        (dim_min_max[dims[4]][0]<full_data[dims[4]])]
+        
+        # cluster_idx = list(cluster_idx)
+        # dist_c=distances[c]
+        # distances_cluster=dist_c[cluster_idx]
+        # max_dist_c=max(distances_cluster)+y
+        # dist_all_c=np.array(distances_all[c])
+        # dataset_idx=np.argwhere(dist_all_c<max_dist_c).squeeze(1).tolist()
+        f_data=rect_data.sample(f)
+        fixed_ind.extend(f_data.index)
+    
+    fixed_ind=np.unique(fixed_ind)
+    print(len(fixed_ind))
+    return fixed_ind
 
 def initial_dataset(df,target_labels):
     labels=[0,0,0,0,0]
@@ -83,14 +133,22 @@ def initial_dataset(df,target_labels):
         dataset=df.iloc[ind]
     return dataset,labels
 
-def iter_dataset(df,target_labels):
+def iter_dataset(df,df_all,target_labels):
     labels=[0]
-    while np.count_nonzero(np.array(labels))==0:
-        new_df=df.drop(['objid'],axis=1)
+    # while np.count_nonzero(np.array(labels))==0:
+    new_df=df.drop(['objid'],axis=1)
+    if len(new_df)<6:
+        ind=find_n_obj_data(new_df,df_all)
+    else:
         km=run_kmeans(new_df,6)
-        ind=find_n_obj_kmeans(km,new_df,10,True)
-        labels=target_labels[ind]
-        dataset=df.iloc[ind]
+        ind=find_n_obj_kmeans(km,new_df,10,df_all,True)
+    
+    if len(ind)>20:
+        print(ind.shape)
+        ind_idx = np.random.choice(ind.shape[0], 20, replace=False)
+        ind=ind[ind_idx]
+    labels=target_labels[ind]
+    dataset=df_all.iloc[ind]
     return dataset,labels
 
 def fit_dc(train_x,train_y):
@@ -103,7 +161,7 @@ def get_metrics(target_labels,pred_labels):
     return f1_score(target_labels,pred_labels)
 
 
-user_query="select * from skyphoto where colc > 25 and colc < 75 and rowc > 20 and rowc < 80;"
+user_query="select * from skyphoto where colc > 25 and colc < 30;"
 
 # target_df=utils.run_query_to_df(user_query)
 # target_objs=target_df['objid'].values
@@ -111,48 +169,78 @@ user_query="select * from skyphoto where colc > 25 and colc < 75 and rowc > 20 a
 target_objs=get_target(user_query)
 
 df=pd.read_csv("./data/small_sdss.csv")
+df_sampler=pd.read_csv("./data/small_sdss.csv")
 all_objid=df['objid'].values
 start_df=df.drop(['objid'],axis=1)
 
 target_labels=get_target_labels(target_objs,df)
 dataset,labels=initial_dataset(df,target_labels)
 
-
 train_x=dataset.drop(['objid'],axis=1)
 dc=fit_dc(train_x,labels)
 
-pred_labels=dc.predict(start_df)
+sample=20
 
-print("Score :",get_metrics(target_labels,pred_labels))
+training_x=df.sample(sample)
+idx=training_x.index
+training_y=target_labels[idx]
+pred_labels=dc.predict(training_x.drop(['objid'],axis=1))
 
-
-for i in range(10):
-    unq = np.array([x + 2*y for x, y in zip(pred_labels, target_labels)])
+print("Score :",get_metrics(training_y,pred_labels))
+dataset=pd.concat([dataset,training_x])
+labels=np.concatenate((labels,training_y))
+for i in range(20):
+    unq = np.array([x + 2*y for x, y in zip(pred_labels, labels)])
     fn = np.array(np.where(unq == 2)).tolist()[0]
     fp = np.array(np.where(unq == 1)).tolist()[0]
 
     fn_fp=fn+fp
-
-    new_dataset=df.iloc[fn_fp]
-    new_labels=target_labels[fn_fp]
+    new_dataset=dataset.iloc[fn_fp]
+    new_labels=labels[fn_fp]
 
     print("Misclassified data :",len(new_dataset))
     print("Misclassified labels :",len(new_labels))
+    # if len(new_dataset)<6:
+    #     break
+    if len(new_dataset)==0:
+        training_x=df.sample(sample)
+        idx=training_x.index
+        training_y=target_labels[idx]
+        dataset=pd.concat([dataset,training_x])
+        labels=np.concatenate((labels,training_y))
+        pred_labels=dc.predict(dataset.drop(['objid'],axis=1))
+        # print("OG LABELS",labels)
+        # print("PRED LABELS",pred_labels)
 
-    new_dataset,new_labels=iter_dataset(new_dataset,new_labels)
+        dc=fit_dc(dataset.drop(['objid'],axis=1),labels)
+
+        pred_labels_full=dc.predict(start_df)
+        score=get_metrics(target_labels,pred_labels_full)
+        print("Length of dataset :",len(dataset))
+        print("Score on full dataset:",score)
+        pred_labels=dc.predict(dataset.drop(['objid'],axis=1))
+        continue
+    new_dataset,new_labels=iter_dataset(new_dataset,start_df,target_labels)
 
     dataset=pd.concat([dataset,new_dataset])
     labels=np.concatenate((labels,new_labels))
 
+    # dataset['label']=labels
+    # no_dup_dataset=dataset.drop_duplicates(subset=['objid'])
+    # labels=no_dup_dataset['label'].values
+    # dataset=no_dup_dataset.drop(['label'],axis=1)
+
+    print("Length of dataset :",len(dataset))
     new_train_x=dataset.drop(['objid'],axis=1)
 
     dc=fit_dc(new_train_x,labels)
 
-    pred_labels=dc.predict(start_df)
-    score=get_metrics(target_labels,pred_labels)
-    print("Score :",get_metrics(target_labels,pred_labels))
+    pred_labels_full=dc.predict(start_df)
+    score=get_metrics(target_labels,pred_labels_full)
+    print("Score on full dataset:",score)
 
-    print(pred_labels)
+    pred_labels=dc.predict(dataset.drop(['objid'],axis=1))
+
     if score==1:
         break
     # pred_labels=np.array(pred_labels)
